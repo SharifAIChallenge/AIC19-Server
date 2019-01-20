fad59090f2e61860e2c3006bfe75bbcdb39355cd
taslimisina <taslimisina@yahoo.com>
2019-01-20 23:43:53 +0330

    Added some functions for GameEngine

--- src/main/java/ir/sharif/aichallenge/server/hamid/controller/GameEngine.java
+++ src/main/java/ir/sharif/aichallenge/server/hamid/controller/GameEngine.java
@@ -1,4 +1,26 @@
 package ir.sharif.aichallenge.server.hamid.controller;
 
+import ir.sharif.aichallenge.server.hamid.model.Player;
+
 public class GameEngine {
+    private Player player1, player2;
+
+    public static void main(String[] args) {
+        GameEngine gameEngine = new GameEngine();
+        gameEngine.initialize();
+        gameEngine.doPickTurn();
+        gameEngine.doTurn();
+    }
+
+    public void initialize() {
+        // todo
+    }
+
+    public void doPickTurn() {
+
+    }
+
+    public void doTurn() {
+
+    }
 }
