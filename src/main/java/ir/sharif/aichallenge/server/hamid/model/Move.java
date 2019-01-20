890fd071cb56e5aaf8adb0565987d7e6f11e823e
seyedalireza <s.hosseini@ce.sharif.edu>
2019-01-21 01:20:30 +0330

    added controllers class

---
+++ src/main/java/ir/sharif/aichallenge/server/hamid/model/Move.java
@@ -0,0 +1,19 @@
+package ir.sharif.aichallenge.server.hamid.model;
+
+
+import ir.sharif.aichallenge.server.hamid.model.enums.Direction;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Move {
+    private List<Direction> moves;
+    private Integer heroId;
+}
