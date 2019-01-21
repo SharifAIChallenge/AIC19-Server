d33506e868d12fc85ba568535eb511160d809325
taslimisina <taslimisina@yahoo.com>
2019-01-21 06:23:28 +0330

    Added prepare and sort for move turn

--- src/main/java/ir/sharif/aichallenge/server/hamid/controller/GameEngine.java
+++ src/main/java/ir/sharif/aichallenge/server/hamid/controller/GameEngine.java
@@ -4,10 +4,8 @@ import ir.sharif.aichallenge.server.hamid.model.*;
 import ir.sharif.aichallenge.server.hamid.model.enums.Direction;
 import ir.sharif.aichallenge.server.hamid.model.enums.GameState;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.Map;
-import java.util.Random;
 
 public class GameEngine {
 
@@ -53,6 +51,16 @@ public class GameEngine {
         //move
         if (state.equals(GameState.MOVE)) {
             //todo sort
+            List<Move> moves1 = message1.getMoves();
+            List<Move> moves2 = message2.getMoves();
+            for (Move move: moves1) {
+                prepareMove(move);
+            }
+            for (Move move: moves2) {
+                prepareMove(move);
+            }
+            moves1.sort(Comparator.comparingInt(o -> o.getMoves().size()));
+            moves2.sort(Comparator.comparingInt(o -> o.getMoves().size()));
             //todo move and vision
         }
 
@@ -64,6 +72,59 @@ public class GameEngine {
 
         //todo check game state
 
+    }
+
+    private void prepareMove(Move move) {
+        Cell cell = move.getHero().getCell(); // todo not change cell
+        List<Direction> newMoves = new ArrayList<>();
+        for (Direction direction: move.getMoves()) {
+            if (isValid(cell, direction)) {
+                cell = nextCell(cell, direction);
+                newMoves.add(direction);
+            }
+        }
+        move.setMoves(newMoves);
+    }
+
+    private boolean isValid(Cell cell, Direction direction) {
+        int column = cell.getColumn();
+        int row = cell.getRow();
+        switch (direction) {
+            case UP:
+                row--;
+                break;
+            case DOWN:
+                row++;
+                break;
+            case LEFT:
+                column--;
+                break;
+            case RIGHT:
+                column++;
+                break;
+        }
+        return (row >= 0 && row < map.getNumberOfRows() && column >= 0
+                && column < map.getNumberOfColumns() && !map.getCells()[row][column].isWall()); // todo 0 based ok?
+    }
+
+    private Cell nextCell(Cell cell, Direction direction) {
+        int column = cell.getColumn();
+        int row = cell.getRow();
+        switch (direction) {
+            case UP:
+                row--;
+                break;
+            case DOWN:
+                row++;
+                break;
+            case LEFT:
+                column--;
+                break;
+            case RIGHT:
+                column++;
+                break;
+        }
+        return map.getCells()[row][column];
     }
 
     private Cell getEmptyCell(int row , int collumn) {
