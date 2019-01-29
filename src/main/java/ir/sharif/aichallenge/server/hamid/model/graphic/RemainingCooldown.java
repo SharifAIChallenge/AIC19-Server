471ec2eb6ccc04d8a4b15020c2204686779c7310
taslimisina <taslimisina@yahoo.com>
2019-01-29 17:44:20 +0330

    Competed getStatusMessage() in GameHandler

--- src/main/java/ir/sharif/aichallenge/server/hamid/model/graphic/RemainingCooldown.java
+++ src/main/java/ir/sharif/aichallenge/server/hamid/model/graphic/RemainingCooldown.java
@@ -1,10 +1,12 @@
 package ir.sharif.aichallenge.server.hamid.model.graphic;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
+@AllArgsConstructor
 public class RemainingCooldown {
     private String ability;
     private int num;
