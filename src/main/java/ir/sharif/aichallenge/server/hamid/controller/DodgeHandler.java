5fd37318c9a34d8a0368362c2f20b219e7c11247
taslimisina <taslimisina@yahoo.com>
2019-01-30 20:50:43 +0330

    Added dodge handler

---
+++ src/main/java/ir/sharif/aichallenge/server/hamid/controller/DodgeHandler.java
@@ -0,0 +1,91 @@
+package ir.sharif.aichallenge.server.hamid.controller;
+
+import ir.sharif.aichallenge.server.hamid.model.*;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class DodgeHandler {
+    private Map map;
+    private List<Cast> dodgeCasts;
+    private Set<Cast> okCasts = new HashSet<>();
+    private Set<Cast> tmpCasts;
+    private Player player;
+    private java.util.Map<Hero, Cell> mainHeroCells = new HashMap<>();
+
+    public DodgeHandler(Map map, Player player, List<Cast> dodgeCasts) {
+        this.map = map;
+        this.player = player;
+        this.dodgeCasts = dodgeCasts;
+    }
+
+    public Set<Cast> getValidDodgeCasts() {
+        fillMainHeroCells();
+        int remainingAp = player.getActionPoint();
+
+        for (Cast cast : dodgeCasts) {
+            if (okCasts.contains(cast))
+                continue;
+            tmpCasts = new HashSet<>();
+            if (dfs(cast, remainingAp)) {
+                for (Cast doneCast : tmpCasts) {
+                    remainingAp -= doneCast.getAbility().getApCost();
+                }
+                okCasts.addAll(tmpCasts);
+            }
+            else {
+                for (Cast wrongCast : tmpCasts) {
+                    resetPlace(wrongCast.getHero());
+                }
+            }
+        }
+
+        for (Hero hero : player.getHeroes()) {
+            resetPlace(hero);
+        }
+
+        return okCasts;
+    }
+
+    private boolean dfs(Cast cast, int ap) {
+        Hero dodgerHero = cast.getHero();
+        Cell endCell = map.getCell(cast.getTargetRow(), cast.getTargetColumn());
+        if (endCell.isWall())   //todo is a valid cell in the map? i.e. not out of map
+            return false;
+        if (cast.getAbility().getApCost() > ap)
+            return false;
+
+        for (Hero hero : player.getHeroes()) {
+            if (hero.getCell() == endCell) {
+                for (Cast nextCast : dodgeCasts) {  //todo dodgeCasts ok?
+                    if (nextCast.getHero() == hero) {
+                        dodgerHero.setCell(endCell);
+                        if (dfs(nextCast, ap - cast.getAbility().getApCost())) {    // then all dodges get ok
+                            tmpCasts.add(cast);
+                            return true;
+                        }
+                        else {
+                            resetPlace(dodgerHero);
+                            return false;       //todo reset hero cell? --> up
+                        }
+                    }
+                }
+            }
+        }
+
+        tmpCasts.add(cast);
+        return true;
+    }
+
+    private void resetPlace(Hero hero) {
+        hero.setCell(mainHeroCells.get(hero));
+    }
+
+    private void fillMainHeroCells() {
+        for (Hero hero : player.getHeroes()) {
+            mainHeroCells.put(hero, hero.getCell());
+        }
+    }
+}
